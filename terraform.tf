d7d9f596a4796e2bd17b52151caad219fb9cfbf8
DaShaun <dashaun@redislabs.com>
2021-10-12 21:15:01 -0500

    update versions

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0.8"
+  required_version = ">= 1.0.0"
   required_providers {
     random = {
       source  = "hashicorp/random"
@@ -7,7 +7,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.79.1"
+      version = "2.67.0"
     }
   }
 }
